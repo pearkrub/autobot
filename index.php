f7dddf4b09301e33c9c3d331cb1e7fe4ffd7d7b8
Praibool Namwong <praibool.piak@gmail.com>
2017-11-02 16:53:08 +0700

    send location

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
     use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
     use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+    use \LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
 
     $channel_token = 'OnTQMCBIsw6hEL6IxCU04LMTqy8jQTdI2TXZhK1xk9+3h4+ZDPb3fSaWsl/ZdNjmmiTzwC8T0SqhZu/vbxsMkAaeT0xj4zptjkkgNXI23CZFIUVi/xGwuVDd3RbztCT5HCn84Lsk5/QREA2p+xkROQdB04t89/1O/w1cDnyilFU='; 
     $channel_secret = '328217598dac9a7d3a70a173e319fbe6'; 
@@ -75,7 +76,7 @@
                         $respMessage = 'Hello, your video ID is '. $messageID; 
                         $originalContentUrl = 'https://www.select2web.com.com/the-fuji.mp4'; 
                         $previewImageUrl = 'https://www.select2web.com.com/the-fuji.jpg'; 
-                        
+
                         $httpClient = new CurlHTTPClient($channel_token); 
                         $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
                         $textMessageBuilder = new VideoMessageBuilder($originalContentUrl, $previewImageUrl); 
@@ -98,6 +99,11 @@
                     case 'location': 
                         $address = $event['message']['address']; // Reply message 
                         $respMessage = 'Hello, your address is '. $address; 
+                        $title = 'I am here'; 
+                        $latitude = $event['message']['latitude']; 
+                        $longitude = $event['message']['longitude'];
+                        $textMessageBuilder = new LocationMessageBuilder($title, $address, $latitude, $longitude); 
+                        $response = $bot->replyMessage($replyToken, $textMessageBuilder);
                         break;
 
                     default: 
