aecdf2e37fc4a870918bca0059e8518fbfc20058
Praibool Namwong <praibool.piak@gmail.com>
2017-11-02 15:24:54 +0700

    send audio

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = 'OnTQMCBIsw6hEL6IxCU04LMTqy8jQTdI2TXZhK1xk9+3h4+ZDPb3fSaWsl/ZdNjmmiTzwC8T0SqhZu/vbxsMkAaeT0xj4zptjkkgNXI23CZFIUVi/xGwuVDd3RbztCT5HCn84Lsk5/QREA2p+xkROQdB04t89/1O/w1cDnyilFU='; 
 $channel_secret = '328217598dac9a7d3a70a173e319fbe6'; 
 
+$httpClient = new CurlHTTPClient($channel_token); 
+$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+
 // Get message from Line API 
 $content = file_get_contents('php://input'); 
 $events = json_decode($content, true); 
@@ -40,8 +43,6 @@ if (!is_null($events['events'])) {
                 case 'video': 
                     $messageID = $event['message']['id']; // Create video file on server. 
                     $fileID = $event['message']['id']; 
-                    $httpClient = new CurlHTTPClient($channel_token); 
-                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
                     $response = $bot->getMessageContent($fileID); 
                     $fileName = 'linebot.mp4'; 
                     $file = fopen($fileName, 'w'); 
@@ -49,12 +50,23 @@ if (!is_null($events['events'])) {
                     $respMessage = 'Hello, your video ID is '. $messageID; 
                     break;
 
+                case 'audio': 
+                    $messageID = $event['message']['id']; 
+                    // Create audio file on server. 
+                    $fileID = $event['message']['id']; 
+                    $response = $bot->getMessageContent($fileID); 
+                    $fileName = 'linebot.m4a'; 
+                    $file = fopen($fileName, 'w'); 
+                    fwrite($file, $response->getRawBody()); 
+                    // Reply message 
+                    $respMessage = 'Hello, your audio ID is '. 
+                    $messageID; 
+                    break;
+                    
                 default: 
                     $respMessage = 'Please send image only'; 
                     break;
             } 
-            $httpClient = new CurlHTTPClient($channel_token); 
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
             $textMessageBuilder = new TextMessageBuilder($respMessage); 
             $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
         } 
