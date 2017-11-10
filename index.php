abf1e87e2a339f157fbf6043752ff48c7ca150b3
Praibool Namwong <praibool.piak@gmail.com>
2017-11-10 12:11:24 +0700

    text send

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
 
     // Get message from Line API 
-    error_log($_GET['uderId']);
+    error_log($_GET);
     $content = file_get_contents('php://input'); 
     $events = json_decode($content, true); 
     error_log(json_encode($events));
